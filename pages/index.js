9863d4827168ab2f1bfd83417f14744a95683d71
ArcaneWright <77860571+arcanewright@users.noreply.github.com>
2022-02-17 07:10:58 -0500

    Started separating out elements

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
 import Head from 'next/head'
 import { Box, Button, Center, Flex, Heading, IconButton, Image, createIcon, Icon, useDisclosure, Modal, ModalOverlay, ModalContent, ModalHeader, ModalCloseButton, ModalBody, ModalFooter } from '@chakra-ui/react'
+import { useState } from 'react';
 
 const Home = () => {
+
+  const [defaultWorkTime, setDefaultBreakTime] = useState(25);
 
   return (
     <Box userSelect="none" minH="100vh">
@@ -14,46 +17,87 @@ const Home = () => {
         <Center>
           <Flex color="whitesmoke" padding="1rem" w="sm"  justify="space-between" align="center">
             <Flex align="center">
-              <Box h="8" w="8" bgImg="/coin.svg" alt="Coin"></Box>
+              <Box h="8" w="8" bgImg="/coin.svg" alt="Coin - Money"></Box>
               <Heading padding=".5rem" size="lg" fontWeight="light">12345</Heading>
             </Flex>
-            <Flex aria-label='energy'>
-              <Box h="8" w="8" bgImg="/lightningEmpty.svg" alt="Empty Energy"></Box>
-              <Box h="8" w="8" bgImg="/lightningEmpty.svg" alt="Empty Energy"></Box>
-              <Box h="8" w="8" bgImg="/lightning.svg" alt="Full Energy"></Box>
-              <Box h="8" w="8" bgImg="/lightning.svg" alt="Full Energy"></Box>
-            </Flex>
+            {createEnergy(4)}
           </Flex>
         </Center>
       </Box>
       <Box>
         <Center>
-          <Heading margin=" 0 0 1rem 0" padding="1rem" size="4xl" bgGradient="linear(to-b, orange.400, red.400)" bgClip="text">Tomato RPG</Heading>
+          <Heading margin=" 0 0 1rem 0" padding="4" size="4xl" bgGradient="linear(to-b, orange.400, red.400)" bgClip="text">Tomato RPG</Heading>
         </Center>
       </Box>
       
       <Box>
         <Center>
-          <Flex w="sm" justify="center" align="center">
-            <Button colorScheme="yellow" h="20" w="24" margin=".5rem"><Box h="12" w="12" bgImg="/basket.svg" alt="Basket"></Box></Button>
-            <Button colorScheme="yellow" h="20" w="24" margin=".5rem"><Box h="12" w="12" bgImg="/blueBook.svg" alt="Blue Book"></Box></Button>
-            <Button colorScheme="yellow" h="20" w="24" margin=".5rem"><Box h="12" w="12" bgImg="/shoppingCart.svg" alt="Shopping Cart"></Box></Button>
-          </Flex>
-        </Center>
-        <Center>
-          <Flex borderRadius="8" w="sm" justify="center">
-            <CreateTimer></CreateTimer>
+          <Flex borderRadius="8" mb="8" w="sm" justify="center" align="center">
+            <CreateTimer defaultTime={defaultWorkTime}></CreateTimer>
             <CreateBreak></CreateBreak>
           </Flex>
         </Center>
+        <Center>
+          <Flex w="sm" justify="center" align="center">
+            <Button colorScheme="yellow" h="20" w="24" margin=".5rem"><Box h="12" w="12" bgImg="/basket.svg" alt="Basket - Inventory"></Box></Button>
+            <Button colorScheme="yellow" h="20" w="24" margin=".5rem"><Box h="12" w="12" bgImg="/blueBook.svg" alt="Blue Book - Recipies"></Box></Button>
+            <Button colorScheme="yellow" h="20" w="24" margin=".5rem"><Box h="12" w="12" bgImg="/shoppingCart.svg" alt="Shopping Cart - Store"></Box></Button>
+          </Flex>
+        </Center>
+        <Center>
+          <Flex w="sm" justify="center" align="center">
+            <Button colorScheme="yellow" h="20" w="24" margin=".5rem"><Box h="12" w="12" alt="Empty"></Box></Button>
+            <Button colorScheme="yellow" h="20" w="24" margin=".5rem"><Box h="12" w="12" alt="Empty"></Box></Button>
+            <Button colorScheme="yellow" h="20" w="24" margin=".5rem"><Box h="12" w="12" bgImg="/gear.svg" alt="Gear - Settings"></Box></Button>
+          </Flex>
+        </Center>
+        
       </Box>
     </Box>
     
   )
 }
 
-const CreateTimer = () => {
+const createEnergy = (num, max = 4) => {
+
+  if (num > max) {
+    throw(Error("Too much energy!"))
+  }
+
+  let empty = [];
+  for (let i = 0; i < max-num; i++) {
+    empty.push(<Box h="8" w="8" bgImg="/lightningEmpty.svg" alt="Empty Energy" key={"empty" + i}></Box>)
+  }
+
+  let full = [];
+  for (let i = 0; i < num; i++) {
+    full.push(<Box h="8" w="8" bgImg="/lightning.svg" alt="Full Energy" key={"full" + i}></Box>)
+  }
+
+  return (
+    <Flex aria-label='energy'>
+      {empty}
+      {full}
+    </Flex>
+  )
+
+}
+
+const CreateTimer = (defaultTime = 25) => {
+  const [time, setTime] = useState(defaultTime);
   const { isOpen, onOpen, onClose } = useDisclosure();
+
+  const timeFormatted = (minutes) => {
+    let result = "";
+    if (minutes >= 10) {
+      result = minutes + ":00"
+    }
+    else {
+      result = "0" + minutes + ":00"
+    }
+
+    return result;
+  }
 
   return (
     <>
@@ -66,10 +110,12 @@ const CreateTimer = () => {
           <ModalBody>
             <Center><Flex align="center" justify="center"><Button></Button><Box h="16" w="16"><Image src="/tomato.svg" alt="tomato"></Image></Box><Button></Button></Flex></Center>
             <Center><Flex align="center" justify="center"><Button></Button><Heading>75:00</Heading><Button></Button></Flex></Center>
+            
+            <Center><Button colorScheme="green" m="4"><Heading p="4">{timeFormatted(time)}</Heading></Button></Center>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="blue" mr="3" onClick={onClose}>Close</Button>
+            <Button colorScheme="red" mr="3" onClick={onClose}>Close</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
@@ -82,18 +128,21 @@ const CreateBreak = () => {
 
   return (
     <>
-      <Button borderRadius="50%" colorScheme="teal" h="32" w="32" margin=".5rem" onClick={onOpen}><Box h="16" w="16" bgImage="/hotBeverage.svg" alt="Hot Beverage"></Box></Button>
+      <Button borderRadius="50%" colorScheme="teal" h="32" w="32" margin=".5rem" onClick={onOpen}><Box h="16" w="16" bgImage="/hotBeverage.svg" alt="Hot Beverage" ></Box></Button>
       <Modal isOpen={isOpen} onClose={onClose}>
         <ModalOverlay></ModalOverlay>
         <ModalContent>
           <ModalHeader>Start Break</ModalHeader>
           <ModalCloseButton></ModalCloseButton>
           <ModalBody>
-            <Heading>Select your vegetable</Heading>
+            <Button colorScheme="teal" m="3" mr="3.5" p="8"><Heading size="lg"> 5 mins</Heading></Button>
+            <Button colorScheme="teal" m="3" p="8"><Heading size="lg">10 mins</Heading></Button>
+            <Button colorScheme="teal" m="3" p="8"><Heading size="lg">15 mins</Heading></Button>
+            <Button colorScheme="teal" m="3" p="8"><Heading size="lg">20 mins</Heading></Button>
           </ModalBody>
 
           <ModalFooter>
-            <Button colorScheme="teal" mr="3" onClick={onClose}>Close</Button>
+            <Button colorScheme="red" mr="3" onClick={onClose}>Close</Button>
           </ModalFooter>
         </ModalContent>
       </Modal>
